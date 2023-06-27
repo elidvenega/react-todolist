1669ebf622c7d9971338bb1bae11ec1ed889a0d1
elidvenega <venegaelid@gmail.com>
2023-06-27 16:45:24 -0700

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+// import "./liststyles.css";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  }
+
+  function handleDelete(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+  return (
+    <>
+      <div>
+        <h1>Todo List</h1>
+        <form>
+          <input type="text" value={inputValue} onChange={handleChange} />
+          <button onClick={handleSubmit}>Add Todo</button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={todo}>
+              {todo}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
